6f202931670b892a48bdd712115b76cd67604690
mailenguyen <mailenguyen2301@gmail.com>
2026-01-21 23:58:23 +0700

    Fix upload images

--- src/main/java/java02/group1/productcatalogmanagementsystem/controller/AdminProductController.java
+++ src/main/java/java02/group1/productcatalogmanagementsystem/controller/AdminProductController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -23,7 +20,7 @@ public class AdminProductController {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ProductResponse> createProduct(
-            @Valid @RequestPart("product") ProductRequest productDTO,
+            @Valid @ModelAttribute ProductRequest productDTO,
             @RequestPart("image") MultipartFile image) {
 
         return ResponseEntity.ok(productService.createProduct(productDTO, image));
--- src/main/java/java02/group1/productcatalogmanagementsystem/dto/response/ProductResponse.java
+++ src/main/java/java02/group1/productcatalogmanagementsystem/dto/response/ProductResponse.java
@@ -1,10 +1,16 @@
 package java02.group1.productcatalogmanagementsystem.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResponse {
     private Long id;
     private String name;
